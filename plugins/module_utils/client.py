a79006ffea727ff18fd9bed683ab4acd362d5d02
Martynas Deveikis <martynasdev@gmail.com>
2024-07-31 16:17:30 +0300

    linting and code documentation

--- plugins/module_utils/client.py
+++ plugins/module_utils/client.py
@@ -1,5 +1,23 @@
+# Copyright: (c) 2024, Cherry Servers UAB <info@cherryservers.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""Cherry Servers public API client.
+
+This utility module is used by ansible modules to access Cherry Servers public API.
+It uses the ansible provided standard utilities for working with HTTP(S).
+
+Classes:
+
+    CherryServersClient
+
+Exceptions:
+
+    ClientError
+
+"""
+
 import json
-from typing import Tuple, Any
+from typing import Any
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import fetch_url
@@ -9,7 +27,19 @@ class ClientError(Exception):
     """A client related error."""
 
 
-class CherryServersClient(object):
+class CherryServersClient:  # pylint: disable=too-few-public-methods
+    """Cherry Server public API client.
+
+    Upon instantiation this class will attempt to validate
+    the provided Cherry Servers authentication token,
+    and will fail the ansible module if unable to do so.
+
+    Methods:
+
+        send_request(method: str, url: str, **kwargs) -> Any
+
+    """
+
     _base_url = "https://api.cherryservers.com/v1/"
 
     def __init__(self, module: AnsibleModule):
@@ -17,21 +47,43 @@ class CherryServersClient(object):
         self._base_url = self._module.params.get(
             "base_url", CherryServersClient._base_url
         )
-        self._bearer_token = self._module.params.get("bearer_token", None)
+        self._auth_token = self._module.params.get("auth_token", None)
+        if self._auth_token is None:
+            self._module.fail_json(msg="auth_token is required.")
+
         self._headers = {
-            "Authorization": f"Bearer {self._bearer_token}",
+            "Authorization": f"Bearer {self._auth_token}",
             "Content-Type": "application/json",
         }
 
-        self._validate_bearer_token()
+        self._validate_auth_token()
 
-    def _validate_bearer_token(self):
+    def _validate_auth_token(self):
         try:
             self.send_request("GET", "user")
-        except ClientError as err:
-            self._module.fail_json(msg="Failed to validate bearer token.", error=err)
+        except ClientError:
+            self._module.fail_json(msg="Failed to validate auth token.")
 
     def send_request(self, method: str, url: str, **kwargs) -> Any:
+        """Send request to Cherry Servers API.
+
+        This sends a request to Cherry Servers API and returns the response payload upon success.
+        If an API error occurs, a ClientError exception is raised with the error payload.
+
+        Args:
+
+            method (str): The HTTP method to use.
+            url (str): The URL to send the request to. Will be appended to the base URL.
+            kwargs (dict): The keyword arguments to pass to the requests.
+
+        Returns:
+
+            Any: The response payload.
+
+        Raises:
+
+            ClientError: The API server responded with an error.
+        """
         url = self._base_url + url
         data = json.dumps(kwargs)
 
@@ -40,7 +92,7 @@ class CherryServersClient(object):
         )
 
         body = resp.read()
-        if info["status"] >= 200:
-            raise ClientError(info["body"])
+        if info["status"] >= 400:
+            raise ClientError(json.loads(info["body"]))
 
         return json.loads(body)
---
+++ plugins/modules/sshkey.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2024, Cherry Servers UAB <info@cherryservers.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type  # __metaclass__ is an exception to standard naming style, so pylint: disable=invalid-name.
+
+DOCUMENTATION = r"""
+---
+module: sshkey
+
+short_description: Create and manage SSH keys on Cherry Servers.
+
+version_added: "0.1.0"
+
+description: Create, update and delete SSH keys on Cherry Servers.
+
+options:
+    auth_token:
+        description:
+            - API authentication token for Cherry Servers public API.
+            - Can be supplied via CHERRY_AUTH_TOKEN and CHERRY_AUTH_KEY environment variables.
+            - See https://portal.cherryservers.com/settings/api-keys for more information.
+        type: str
+    state:
+        description:
+            - The state of the SSH key.
+        default: present
+        choices: ['absent', 'present']
+        type: str
+    public_key:
+        description:
+            - The public SSH key.
+            - Required if the key doesn't exist.
+        type: str
+    label:
+        description:
+            - The label of the SSH key.
+        aliases: [name]
+        type: str
+# Specify this value according to your collection
+# in format of namespace.collection.doc_fragment_name
+# extends_documentation_fragment:
+#     - my_namespace.my_collection.my_doc_fragment_name
+
+author:
+    - Martynas Deveikis (@caliban0)
+"""
+
+EXAMPLES = r"""
+- name: "Create SSH key"
+  local.cherryservers.sshkey:
+    auth_token: "{{ auth_token }}"
+    name: "SSH test key"
+    public_key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYe+GfpsnLP02tfLOJWWFnGKJNpgrzLYE5VZhclrFy0 example@example.com"
+    state: present
+  register: result
+"""
+
+RETURN = r"""
+# Cherry Servers API reference: https://api.cherryservers.com/doc/#tag/SshKeys
+original_message:
+    description: The original name param that was passed in.
+    type: str
+    returned: always
+    sample: 'hello world'
+message:
+    description: The output message that the test module generates.
+    type: str
+    returned: 
+    sample: 'goodbye'
+cherryservers_sshkey:
+    description: SSH key data.
+    returned: when C(state=present)
+    type: dict
+    sample: {
+        "cherryservers_ssh_key": {
+            "created": "2024-07-31T05:33:08+00:00",
+            "fingerprint": "54:8e:84:11:bb:29:59:41:36:cb:e2:k2:0c:4a:77:1d",
+            "href": "/ssh-keys/0000",
+            "id": 0000,
+            "key": "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYe+GfpsnLP02tfLOJWWFnGKJNpgrzLYE5VZhclrFy0 example@example.com",
+            "label": "ansible-test",
+            "updated": "2024-07-31T05:33:08+00:00"
+        }
+    }
+"""
+
+# from ..module_utils import client
+from ansible.module_utils import basic as utils
+from ..module_utils import client
+
+
+def run_module():
+    """Execute the ansible module."""
+
+    module_args = {
+        "auth_token": {
+            "type": "str",
+            "no_log": True,
+            "fallback": (utils.env_fallback, ["CHERRY_AUTH_TOKEN", "CHERRY_AUTH_KEY"]),
+        },
+        "state": {
+            "choices": ["absent", "present"],
+            "default": "present",
+            "type": "str",
+        },
+        "label": {
+            "type": "str",
+            "required": True,
+            "aliases": ["name"],
+        },
+        "public_key": {
+            "type": "str",
+            "required": True,
+        },
+    }
+
+    module = utils.AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+    api_client = client.CherryServersClient(module)
+
+    create_key(api_client, module)
+
+
+def create_key(api_client: client.CherryServersClient, module: utils.AnsibleModule):
+    """Create a new SSH key."""
+    try:
+        resp = api_client.send_request(
+            "POST",
+            "ssh-keys",
+            label=module.params["label"],
+            key=module.params["public_key"],
+        )
+        module.exit_json(changed=True, cherryservers_sshkey=resp)
+    except client.ClientError as e:
+        module.fail_json(msg=str(e))
+
+
+def main():
+    """Main function."""
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
