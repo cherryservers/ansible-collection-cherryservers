f5300cdcff515d38c9e11e284531e8537198563b
Martynas Deveikis <martynasdev@gmail.com>
2026-02-11 09:05:30 +0200

    chore: update major black version and lint according to new rules

--- plugins/module_utils/resource_managers/server_manager.py
+++ plugins/module_utils/resource_managers/server_manager.py
@@ -1,6 +1,7 @@
 # Copyright: (c) 2024, Cherry Servers UAB <info@cherryservers.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """Manage Cherry Servers server resources."""
+
 import time
 from typing import Optional, List
 
